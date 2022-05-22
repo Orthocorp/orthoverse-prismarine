dbbd08cc5d2c5d3ca883ef41688e0e7809901e86
Keir Finlow-Bates <keirfb@gmail.com>
2022-05-22 11:10:25 +0300

    Colored henges

--- patch-files/flying-squid/src/plugins/hengeportals.js
+++ patch-files/flying-squid/src/plugins/hengeportals.js
@@ -5,48 +5,47 @@ const Vec3 = require('vec3').Vec3;
 module.exports.player = function(player, serv) { 
 
   player.on('move', ({position}, cancelled) => { 
-        //orthohenge
+  //orthohenge
+    const jump = 30*6*16; // 2880
+    let xStor = player.position.x;
+    let zStor = player.position.z;
+
+    for (let x=-jump; x<=jump;x=x+jump) {
+      for (let z=-jump; z<=jump;z=z+jump) {
         // north
-        if (((Math.floor(player.position.x) === 47) || (Math.floor(player.position.x) == 48)) &&     
-            (Math.floor(player.position.z) === 32)) {
-          player.teleport(new Vec3(47.5, player.position.y + 1, 47.5 - 30*6*16));
+        if (((Math.floor(xStor) === 47+x) || (Math.floor(xStor) === 48+x)) &&     
+            (Math.floor(zStor) === 32+z)) {
+          zStor = zStor + 15.5 - jump;
+          if (zStor < -3000 ) {zStor = zStor + 3*jump}
+          player.teleport(new Vec3(xStor, player.position.y + 1.5, zStor));
+          z = x = jump + 1;
         }
         // south
-        if (((Math.floor(player.position.x) === 47) || (Math.floor(player.position.x) == 48)) &&     
-            (Math.floor(player.position.z) === 63)) {
-                          player.teleport(new Vec3(47.5, player.position.y + 1, 47.5 + 30*6*16));
+        if (((Math.floor(xStor) === 47+x) || (Math.floor(xStor) === 48+x)) &&     
+            (Math.floor(zStor) === 63+z)) {
+          zStor = zStor - 15.5 + jump;
+          if (zStor > 3000) {zStor = zStor - 3*jump}
+          player.teleport(new Vec3(xStor, player.position.y + 1.5, zStor));
+          z = x = jump + 1;
         }
         // west
-        if (((Math.floor(player.position.z) === 47) || (Math.floor(player.position.z) == 48)) &&     
-            (Math.floor(player.position.x) === 32)) {
-          player.teleport(new Vec3(47.5 - 30*6*16, player.position.y + 1, 47.5));
+        if (((Math.floor(zStor) === 47+z) || (Math.floor(zStor) === 48+z)) &&     
+            (Math.floor(xStor) === 32+x)) {
+          xStor = xStor + 15.5 - jump;
+          if (xStor < -3000) {xStor = xStor + 3*jump}
+          player.teleport(new Vec3(xStor, player.position.y + 1.5, zStor));
+          z = x = jump + 1;
         }
         // east
-        if (((Math.floor(player.position.z) === 47) || (Math.floor(player.position.z) == 48)) &&     
-            (Math.floor(player.position.x) === 63)) {
-          player.teleport(new Vec3(47.5 + 30*6*16, player.position.y + 1, 47.5));
+        if (((Math.floor(zStor) === 47+z) || (Math.floor(zStor) === 48+z)) &&     
+            (Math.floor(xStor) === 63+x)) {
+          xStor = xStor -15.5 + jump;
+          if (xStor > 3000) {xStor = xStor - 3*jump}
+          player.teleport(new Vec3(xStor, player.position.y + 1.5, zStor));
+          z = x = jump + 1;
         }
-        // and back
-        // north
-        if (((Math.floor(player.position.x) === 47) || (Math.floor(player.position.x) == 48)) &&     
-            (Math.floor(player.position.z) === 63 - 30*6*16)) {
-          player.teleport(new Vec3(47.5, player.position.y + 1, 47.5));
-        }
-        // south
-        if (((Math.floor(player.position.x) === 47) || (Math.floor(player.position.x) == 48)) &&     
-            (Math.floor(player.position.z) === 32 + 30*6*16)) {
-                         player.teleport(new Vec3(47.5, player.position.y + 1, 47.5));
-        }
-        // west
-        if (((Math.floor(player.position.z) === 47) || (Math.floor(player.position.z) == 48)) &&     
-            (Math.floor(player.position.x) === 63 - 30*6*16)) {
-          player.teleport(new Vec3(47.5, player.position.y + 1, 47.5));
-        }
-        // east
-        if (((Math.floor(player.position.z) === 47) || (Math.floor(player.position.z) == 48)) &&     
-            (Math.floor(player.position.x) === 32 + 30*6*16)) {
-          player.teleport(new Vec3(47.5, player.position.y + 1, 47.5));
-        }
+      }
+    }
   })
 
 }
